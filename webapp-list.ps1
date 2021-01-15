16bc92777cc9acf359084b8f1d1d223ed77b046d
brnwn4 <62909730+brnwn4@users.noreply.github.com>
2021-01-15 15:27:29 -0500

    Create webapp-list.ps1

---
+++ webapp-list.ps1
@@ -0,0 +1,25 @@
+<#
+Author: Brandon Wong
+Description: This script will allow all app services in a tenant to be queried against for ID, Name, RG, and HostNames to a CSV
+#>
+
+[CmdletBinding()]
+param(
+  $tenant
+)
+
+# Create CSV Headers
+Add-Content -Path ./report.csv -Value "NAME,ID,RESOURCEGROUP,ENABLEDHOSTNAMES"
+
+$tenant = "f7f66891-a582-418d-999e-cb1be5354253"
+
+$subs = @()
+$subs = az account list --query "[?tenantId=='$tenant']" | ConvertFrom-Json
+
+foreach ($sub in $subs) {
+    az account set --subscription $sub.id
+    $apps = az webapp list --query '[].{id:id, name:name, resourceGroup:resourceGroup, enabledHostNames:enabledHostNames}' --output json | ConvertFrom-Json
+    foreach ($app in $apps){
+        Add-Content -Path ./report.csv -Value "$($app.name),$($app.id),$($app.resourcegroup),$($app.enabledHostNames)"
+    } 
+}
